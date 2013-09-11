cda386b038294240784f39fff9a800ac08929af4
Stephen Warren <swarren@nvidia.com>
2013-09-11 17:20:04 -0600

    Add Venice2 configuration
    Signed-off-by: Stephen Warren <swarren@nvidia.com>

---
+++ tegra124/nvidia/venice2/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Copyright (c) 2013, NVIDIA CORPORATION.  All rights reserved.
+#
+# This software is provided 'as-is', without any express or implied
+# warranty. In no event will the authors be held liable for any damages
+# arising from the use of this software.
+#
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+#
+# 1. The origin of this software must not be misrepresented; you must not
+#    claim that you wrote the original software. If you use this software
+#    in a product, an acknowledgment in the product documentation would be
+#    appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+#    misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+
+set -e
+set -x
+
+cbootimage -t124 -gbct \
+    PM371_Hynix_2GB_H5TC4G63AFR_RDA_792MHz_0719.bct.cfg \
+    PM371_Hynix_2GB_H5TC4G63AFR_RDA_792MHz_0719.bct
+cbootimage -t124 venice2-spi.img.cfg venice2-spi.img
